aeeb608b61f3dabcb60972fdd6a21ef327420f4d
omeryuceer <omeryuceer34@hotmail.com>
2025-03-03 10:49:42 +0300

    Add files via upload

---
+++ h3_skor.py
@@ -0,0 +1,13 @@
+import random
+
+fb = random.randint(1, 5)
+print(fb)
+gs = random.randint(1, 5)
+print(gs)
+if fb>gs:
+    print("fb kazandi")
+elif gs>fb:
+    print("gs kazandi")
+else:
+    print("berabere")
+
